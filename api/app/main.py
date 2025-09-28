9c65b2411dac3ff8cf39692efa9f46cbfcbede23
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-09-28 10:19:48 -0400

    Add CORS to localhost

--- api/app/main.py
+++ api/app/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import Response, JSONResponse
 
 from db import get_stored_articles
 
 app = FastAPI(title='ActuSafe')
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
+    allow_methods=['GET'],
+)
 
 @app.get('/health')
 async def health():
